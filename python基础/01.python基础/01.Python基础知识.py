1dd71de392d41aa311c1f594d62362873cd6ede9
eagle2015 <uttflying@foxmail.com>
2017-02-19 20:22:55 +0800

    python 条件语句

--- python基础/01.python基础/01.Python基础知识.py
+++ python基础/01.python基础/01.Python基础知识.py
@@ -141,4 +141,271 @@ print tinydict  # 输出完整的字典
 print tinydict.keys()  # 输出所有键
 print tinydict.values()  # 输出所有值
 
+Python数字
+Python 数字数据类型用于存储数值。
+
+数据类型是不允许改变的,这就意味着如果改变数字数据类型得值，将重新分配内存空间。
+Python 支持四种不同的数值类型
+1、整型(Int)
+      通常被称为是整型或整数，是正或负整数，不带小数点。
+2、长整型(long integers)
+      无限大小的整数，整数最后是一个大写或小写的L。
+3、浮点型(floating point real values)
+      浮点型由整数部分与小数部分组成，浮点型也可以使用科学计数法表示（2.5e2 = 2.5 x 102 = 250）
+4、复数( (complex numbers))
+      复数的虚部以字母J 或 j结尾 。如：2+3i
+
+注意：
+1、长整型也可以使用小写"L"，但是还是建议您使用大写"L"，避免与数字"1"混淆。Python使用"L"来显示长整型。
+2、Python还支持复数，复数由实数部分和虚数部分构成，可以用a + bj,或者complex(a,b)表示， 复数的实部a和虚部b都是浮点型
+
+三、Python数字类型转换
+int(x [,base ])         将x转换为一个整数
+long(x [,base ])        将x转换为一个长整数
+float(x )               将x转换到一个浮点数
+complex(real [,imag ])  创建一个复数
+str(x )                 将对象 x 转换为字符串
+repr(x )                将对象 x 转换为表达式字符串
+eval(str )              用来计算在字符串中的有效Python表达式,并返回一个对象
+tuple(s )               将序列 s 转换为一个元组
+list(s )                将序列 s 转换为一个列表
+chr(x )                 将一个整数转换为一个字符
+unichr(x )              将一个整数转换为Unicode字符
+ord(x )                 将一个字符转换为它的整数值
+hex(x )                 将一个整数转换为一个十六进制字符串
+oct(x )                 将一个整数转换为一个八进制字符串
+
+
+Python数学函数
+函数	返回值 ( 描述 )
+abs(x)	返回数字的绝对值，如abs(-10) 返回 10
+ceil(x)	返回数字的上入整数，如math.ceil(4.1) 返回 5
+cmp(x, y)	如果 x < y 返回 -1, 如果 x == y 返回 0, 如果 x > y 返回 1
+exp(x)	返回e的x次幂(ex),如math.exp(1) 返回2.718281828459045
+fabs(x)	返回数字的绝对值，如math.fabs(-10) 返回10.0
+floor(x)	返回数字的下舍整数，如math.floor(4.9)返回 4
+log(x)	如math.log(math.e)返回1.0,math.log(100,10)返回2.0
+log10(x)	返回以10为基数的x的对数，如math.log10(100)返回 2.0
+max(x1, x2,...)	返回给定参数的最大值，参数可以为序列。
+min(x1, x2,...)	返回给定参数的最小值，参数可以为序列。
+modf(x)	返回x的整数部分与小数部分，两部分的数值符号与x相同，整数部分以浮点型表示。
+pow(x, y)	x**y 运算后的值。
+round(x [,n])	返回浮点数x的四舍五入值，如给出n值，则代表舍入到小数点后的位数。
+sqrt(x)	返回数字x的平方根，数字可以为负数，返回类型为实数，如math.sqrt(4)返回 2+0j
+
+Python随机数函数
+        随机数可以用于数学，游戏，安全等领域中，还经常被嵌入到算法中，用以提高算法效率，并提高程序的安全性。
+        Python包含以下常用随机数函数：
+函数	 描述
+choice(seq)	从序列的元素中随机挑选一个元素，比如random.choice(range(10))，从0到9中随机挑选一个整数。
+randrange ([start,] stop [,step])	从指定范围内，按指定基数递增的集合中获取一个随机数，基数缺省值为1
+random()	随机生成下一个实数，它在[0,1)范围内。
+seed([x])	改变随机数生成器的种子seed。如果你不了解其原理，你不必特别去设定seed，Python会帮你选择seed。
+shuffle(lst)	将序列的所有元素随机排序
+uniform(x, y)	随机生成下一个实数，它在[x,y]范围内。
+
+
+Python三角函数
+Python包括以下三角函数：
+函数	描述
+acos(x)	返回x的反余弦弧度值。
+asin(x)	返回x的反正弦弧度值。
+atan(x)	返回x的反正切弧度值。
+atan2(y, x)	返回给定的 X 及 Y 坐标值的反正切值。
+cos(x)	返回x的弧度的余弦值。
+hypot(x, y)	返回欧几里德范数 sqrt(x*x + y*y)。
+sin(x)	返回的x弧度的正弦值。
+tan(x)	返回x弧度的正切值。
+degrees(x)	将弧度转换为角度,如math.degrees(math.tan(1.0)) ,返回30.0
+radians(x)	将角度转换为弧度
+七、Python数学常量
+常量	描述
+pi	数学常量 pi（圆周率，一般以π来表示）
+e	数学常量 e，e即自然常数（自然常数）。
+
+Python字符串
+字符串是最 Python 总常用的数据类型。我们可以使用引号来创建字符串。
+创建字符串很简单，只要为变量分配一个值即可。
+
+二、Python访问字符串中的值
+Python不支持单字符类型，单字符也在Python也是作为一个字符串使用。
+Python访问子字符串，可以使用方括号来截取字符串，
+
+'''
+#!/usr/bin/python
+
+var1 = 'Hello World!python 2017'
+var2 = "Python Programming"
+
+print "var1[0]: ", var1[0]
+print "var2[1:6]: ", var2[1:6]
+'''
+
+
+三、Python字符串更新
+可以对已存在的字符串进行修改，并赋值给另一个变量，如下实例:
+
+'''
+
+#!/usr/bin/python
+
+var1 = 'Hello World!'
+print "Updated String :- ", var1[:6] + 'Python'
+'''
+'''
+实例执行结果:
+Updated String :-  Hello Python
+'''
+
+Python转义字符
+在需要在字符中使用特殊字符时，python用反斜杠(\)转义字符。如下表：
+转义字符	描述
+\(在行尾时)	续行符
+\\	反斜杠符号
+\'	单引号
+\"	双引号
+\a	响铃
+\b	退格(Backspace)
+\e	转义
+\000	空
+\n	换行
+\v	纵向制表符
+\t	横向制表符
+\r	回车
+\f	换页
+\oyy	八进制数yy代表的字符，例如：\o12代表换行
+\xyy	十进制数yy代表的字符，例如：\x0a代表换行
+\other	其它的字符以普通格式输出
+
+
+Python字符串运算符:
+
+操作符	描述	实例
++	字符串连接	a + b 输出结果： HelloPython
+*	重复输出字符串	a*2 输出结果：HelloHello
+[]	通过索引获取字符串中字符	a[1] 输出结果 e
+[ : ]	截取字符串中的一部分	a[1:4] 输出结果 ell
+in	成员运算符 - 如果字符串中包含给定的字符返回 True	H in a 输出结果 1
+not in	成员运算符 - 如果字符串中不包含给定的字符返回 True	M not in a 输出结果 1
+r/R	原始字符串 - 原始字符串：所有的字符串都是直接按照字面的意思来使用，没有转义特殊或不能打印的字符。
+    原始字符串除在字符串的第一个引号前加上字母"r"（可以大小写）以外，与普通字符串有着几乎完全相同的语法。
+    print r'\n' prints \n 和 print R'\n'prints \n
+%	格式字符串
+
+Python字符串格式化
+       Python 支持格式化字符串的输出 。尽管这样可能会用到非常复杂的表达式，但最基本的用法是将一个值插入到一个有字符串格式符 %s 的字符串中。
+
+'''
+#!/usr/bin/python
+
+print "My name is %s and weight is %d kg!" % ('flying', 51)
+
+
+'''
+
+python字符串格式化符号:
+
+符   号	描述
+      %c	 格式化字符及其ASCII码
+      %s	 格式化字符串
+      %d	 格式化整数
+      %u	 格式化无符号整型
+      %o	 格式化无符号八进制数
+      %x	 格式化无符号十六进制数
+      %X	 格式化无符号十六进制数（大写）
+      %f	 格式化浮点数字，可指定小数点后的精度
+      %e	 用科学计数法格式化浮点数
+      %E	 作用同%e，用科学计数法格式化浮点数
+      %g	 根据值的大小决定使用%f活%e
+      %G	 作用同%g，根据值的大小决定使用%f活%e
+      %p	 用十六进制数格式化变量的地址
+
+格式化操作符辅助指令:
+符号	功能
+*	定义宽度或者小数点精度
+-	用做左对齐
++	在正数前面显示加号( + )
+<sp>	在正数前面显示空格
+#	在八进制数前面显示零('0')，在十六进制前面显示'0x'或者'0X'(取决于用的是'x'还是'X')
+0	显示的数字前面填充'0'而不是默认的空格
+%	'%%'输出一个单一的'%'
+(var)	映射变量(字典参数)
+m.n.	m 是显示的最小总宽度,n 是小数点后的位数(如果可用的话)
+
+八、Python三引号（triple quotes）
+python中三引号可以将复杂的字符串进行复制
+python三引号允许一个字符串跨多行，字符串中可以包含换行符、制表符以及其他特殊字符。
+三引号的语法是一对连续的单引号或者双引号（通常都是成对的用）。
+
+三引号让程序员从引号和特殊字符串的泥潭里面解脱出来，自始至终保持一小块字符串的格式是所谓的WYSIWYG（所见即所得）格式的。
+        一个典型的用例是，当你需要一块HTML或者SQL时，这时用字符串组合，特殊字符串转义将会非常的繁琐。
+
+Unicode 字符串
+      Python 中定义一个 Unicode 字符串和定义一个普通字符串一样简单：
+
+>>> u'Hello World !'
+u'Hello World !'
+
+
+引号前小写的"u"表示这里创建的是一个 Unicode 字符串。如果你想加入一个特殊字符，可以使用 Python 的 Unicode-Escape 编码。如下例所示：
+>>> u'Hello\u0020World !'
+u'Hello World !'
+
+被替换的 \u0020 标识表示在给定位置插入编码值为 0x0020 的 Unicode 字符（空格符）。
+
+
+python的字符串内建函数
+        字符串方法是从python1.6到2.0慢慢加进来的——它们也被加到了Jython中。
+        这些方法实现了string模块的大部分方法，如下表所示列出了目前字符串内建支持的方法，所有的方法都包含了对Unicode的支持，有一些甚至是专门用于Unicode的。
+
+
+
+方法	描述
+string.capitalize()  把字符串的第一个字符大写
+string.center(width) 返回一个原字符串居中,并使用空格填充至长度 width 的新字符串
+string.count(str, beg=0, end=len(string)) 返回 str 在 string 里面出现的次数，如果 beg 或者 end 指定则返回指定范围内 str 出现的次数
+string.decode(encoding='UTF-8', errors='strict') 以encoding指定的编码格式解码string,如果出错默认报一个ValueError的异常,除非errors指定的是'ignore'或者'replace'
+string.encode(encoding='UTF-8', errors='strict') 以encoding指定的编码格式编码string，如果出错默认报一个ValueError的异常，除非errors指定的是'ignore'或者'replace'
+string.endswith(obj, beg=0, end=len(string))检查字符串是否以 obj 结束，如果beg 或者 end 指定则检查指定的范围内是否以 obj 结束，如果是，返回 True,否则返回 False.
+string.expandtabs(tabsize=8) 把字符串 string 中的 tab 符号转为空格，默认的空格数 tabsize 是 8.
+string.find(str, beg=0, end=len(string))检测 str 是否包含在 string 中，如果 beg和 end指定范围，则检查是否包含在指定范围内，如果是返回开始的索引值，否则返回-1
+string.index(str, beg=0, end=len(string)) 跟find()方法一样，只不过如果str不在 string中会报一个异常.
+string.isalnum() 如果 string 至少有一个字符并且所有字符都是字母或数字则返回 True,否则返回 False
+string.isalpha() 如果 string 至少有一个字符并且所有字符都是字母则返回 True,否则返回 False
+string.isdecimal() 如果 string 只包含十进制数字则返回 True 否则返回 False.
+string.isdigit() 如果 string 只包含数字则返回 True 否则返回 False.
+string.islower() 如果 string 中包含至少一个区分大小写的字符，并且所有这些(区分大小写的)字符都是小写，则返回 True，否则返回 False
+string.isnumeric() 如果 string 中只包含数字字符，则返回 True，否则返回 False
+string.isspace() 如果 string 中只包含空格，则返回 True，否则返回 False.
+string.istitle() 如果 string 是标题化的(见 title())则返回 True，否则返回 False
+string.isupper() 如果 string 中包含至少一个区分大小写的字符，并且所有这些(区分大小写的)字符都是大写，则返回 True，否则返回 False
+string.join(seq) Merges (concatenates)以 string 作为分隔符，将 seq 中所有的元素(的字符串表示)合并为一个新的字符串
+string.ljust(width) 返回一个原字符串左对齐,并使用空格填充至长度 width 的新字符串
+string.lower() 转换 string 中所有大写字符为小写.
+string.lstrip() 截掉 string 左边的空格
+string.maketrans(intab, outtab])
+maketrans() 方法用于创建字符映射的转换表，对于接受两个参数的最简单的调用方式，第一个参数是字符串，表示需要转换的字符，第二个参数也是字符串表示转换的目标。
+max(str) 返回字符串 str 中最大的字母。
+min(str) 返回字符串 str 中最小的字母。
+string.partition(str)有点像 find()和 split()的结合体,从 str 出现的第一个位置起,把字符串string分成一个3元素的元组 (string_pre_str,str,string_post_str),
+                                                   如果 string 中不包含str 则 string_pre_str == string.
+
+string.replace(str1, str2,  num=string.count(str1)) 把 string 中的 str1 替换成 str2,如果 num 指定，则替换不超过 num 次.
+
+string.rfind(str, beg=0,end=len(string) )类似于 find()函数，不过是从右边开始查找.
+string.rindex( str, beg=0,end=len(string))类似于 index()，不过是从右边开始.
+string.rjust(width) 返回一个原字符串右对齐,并使用空格填充至长度 width 的新字符串
+string.rpartition(str)类似于 partition()函数,不过是从右边开始查找.
+string.rstrip()删除 string 字符串末尾的空格.
+string.split(str="", num=string.count(str))以 str 为分隔符切片 string，如果 num有指定值，则仅分隔 num 个子字符串
+string.splitlines(num=string.count('\n'))按照行分隔，返回一个包含各行作为元素的列表，如果 num 指定则仅切片 num 个行.
+string.startswith(obj, beg=0,end=len(string))检查字符串是否是以 obj 开头，是则返回 True，否则返回 False。如果beg 和 end 指定值，则在指定范围内检查.
+string.strip([obj])在 string 上执行 lstrip()和 rstrip()
+string.swapcase()翻转 string 中的大小写
+string.title()返回"标题化"的 string,就是说所有单词都是以大写开始，其余字母均为小写(见 istitle())
+string.translate(str, del="")根据 str 给出的表(包含 256 个字符)转换 string 的字符,要过滤掉的字符放到 del 参数中
+string.upper()转换 string 中的小写字母为大写
+string.zfill(width)返回长度为 width 的字符串，原字符串 string 右对齐，前面填充0
+string.isdecimal()  isdecimal()方法检查字符串是否只包含十进制字符。这种方法只存在于unicode对象。
+
 
--- python基础/01.python基础/5.python条件语句,for while 循环语句，循环控制语句.py
+++ python基础/01.python基础/5.python条件语句,for while 循环语句，循环控制语句.py
@@ -110,11 +110,128 @@ Python for  循环语句
 
     一、循环语句的一般形式:
     循环语句允许我们执行一个语句或语句组多次，
+    Python for循环可以遍历任何序列的项目，如一个列表或者一个字符串。
 
-    二、Python的for循环和while循环
+    一、Python for循环语法
+    Python  for循环的语法格式如下：
+    for iterating_var in sequence:
+        statements(s)
+
+'''
+#!/usr/bin/python
+
+for letter in 'Python':     # First Example
+   print 'Current Letter :', letter
+
+fruits = ['banana', 'apple',  'mango','orage']
+for fruit in fruits:        # Second Example
+   print 'Current fruit :', fruit
+
+print "Good bye!"
+'''
+
+三、通过序列索引迭代
+另外一种执行循环的遍历方式是通过索引，如下实例：
+'''
+#!/usr/bin/python
+
+fruits = ['banana', 'apple',  'mango']
+for index in range(len(fruits)):
+   print 'Current fruit :', fruits[index]
+
+print "Good bye!"
+'''
+说明：
+       以上实例我们使用了内置函数 len() 和 range(),函数 len() 返回列表的长度，即元素的个数。 range返回一个序列的数。
+
+
+四、循环使用 else 语句
+       在 python 中，for … else 表示这样的意思，for 中的语句和普通的没有区别，else 中的语句会在循环正常执行完（即 for 不是通过 break 跳出而中断的）的情况下执行，while … else 也是一样。
+如下实例：
+
+
+'''
+#!/usr/bin/python
+
+for num in range(11,20):  #to iterate between 11 to 20
+   for i in range(2,num): #to iterate on the factors of the number
+      if num%i == 0:      #to determine the first factor
+         j=num/i          #to calculate the second factor
+         print '%d equals = %d * %d' % (num,i,j)
+         break #to move to the next number, the #first FOR
+   else:                  # else part of the loop
+      print num, 'is a prime number'
+
+'''
+
+
+'''
+
+11 is a prime number
+12 equals = 2 * 6
+13 is a prime number
+14 equals = 2 * 7
+15 equals = 3 * 5
+16 equals = 2 * 8
+17 is a prime number
+18 equals = 2 * 9
+19 is a prime number
+'''
+
+Python for 循环嵌套语法:
+所谓Python 循环嵌套，是指Python 语言允许在一个循环体里面嵌入另一个循环。
+一、Python for 循环嵌套语法（一）
+
+for iterating_var in sequence:
+    for iterating_var in sequence:
+        statements(s)
+    statements(s)
+
+
+二、Python while 循环嵌套语法（二）
+while expression:
+    while expression:
+        statement(s)
+    statement(s)
+
+你可以在循环体内嵌入其他的循环体，如在while循环中可以嵌入for循环， 反之，你可以在for循环中嵌入while循环。
+
+三、实例
+以下实例使用了嵌套循环输出1~50之间的素数：
+i = 1
+while (i < 50):
+    j = 2
+    while (j <= (i / j)):
+        if not (i % j): break
+        j = j + 1
+    if (j > i / j): print i, " 是素数"
+    i = i + 1
+
+print "Good bye! python 2017"
+
+'''
+1  是素数
+2  是素数
+3  是素数
+5  是素数
+7  是素数
+11  是素数
+13  是素数
+17  是素数
+19  是素数
+23  是素数
+29  是素数
+31  是素数
+37  是素数
+41  是素数
+43  是素数
+47  是素数
+'''
+
+
+二、Python的for循环和while循环
     Python提供了for循环和while循环（在Python中没有do..while循环）:
-    循环类型
-    描述
+    循环类型     描述
     while 循环    在给定的判断条件为 true 时执行循环体，否则退出循环体。
     for 循环       重复执行语句
     嵌套循环       你可以在while循环体中嵌套for循环
@@ -122,9 +239,61 @@ Python for  循环语句
 三、循环控制语句
 循环控制语句可以更改语句执行的顺序。Python支持以下循环控制语句：
 控制语句	描述
-break 语句	在语句块执行过程中终止循环，并且跳出整个循环
+break 语句	    在语句块执行过程中终止循环，并且跳出整个循环
 continue 语句	在语句块执行过程中终止当前循环，跳出该次循环，执行下一次循环。
-pass 语句	pass是空语句，是为了保持程序结构的完整性。
+pass 语句	    pass是空语句，是为了保持程序结构的完整性。
+
+Python break 语句
+Python break语句，就像在C语言中，打破了最小封闭for或while循环。
+break语句用来终止循环语句，即循环条件没有False条件或者序列还没被完全递归完，也会停止执行循环语句。break语句用在while和for循环中。
+如果您使用嵌套循环，break语句将停止执行最深层的循环，并开始执行下一行代码。
+
+'''
+for letter in 'Python':  # First Example
+    if letter == 'o':
+        break
+    print 'Current Letter :', letter
+
+var = 100  # Second Example
+while var > 0:
+    var = var - 10
+    if var == 60:
+        continue
+        # "var=60,continue"
+    elif var == 50:
+        pass
+        print 'This is pass block'
+    elif var == 20:
+        break
+    print 'Current variable value :', var
+print "Good bye!python 2017"
+'''
+
+Python continue 语句
+Python continue 语句跳出本次循环，而break跳出整个循环。continue 语句用来告诉Python跳过当前循环的剩余语句，然后继续进行下一轮循环。
+       continue语句用在while和for循环中。
+
+'''
+Current Letter : P
+Current Letter : y
+Current Letter : t
+Current Letter : h
+Current variable value : 90
+Current variable value : 80
+Current variable value : 70
+Current variable value : 50
+Current variable value : 40
+Current variable value : 30
+Good bye!python 2017
+'''
+
+
+ython pass是空语句，是为了保持程序结构的完整性。
+
+一、Python 语言 pass 语句语法格式
+pass
+
+
 
 
 流程控制
@@ -157,6 +326,95 @@ if else 语句 实现逻辑控制
                 print 'not an age'
 
 
+
+Python While循环语句
+一、Python While循环的基本形式
+Python 编程中 while 语句用于循环执行程序，即在某条件下，循环执行某段程序，以处理需要重复处理的相同任务。
+其基本形式为：
+while 判断条件：
+    执行语句……
+执行语句可以是单个语句或语句块。
+
+二、Python While循环的执行流程图
+判断条件可以是任何表达式，任何非零、或非空（null）的值均为true。
+当判断条件假false时，循环结束。
+'''
+#!/usr/bin/python
+
+count = 0
+while (count < 10):
+   print 'The count is:', count
+   count = count + 1
+print "Good bye!"
+'''
+
+2、continue和break
+while 语句时还有另外两个重要的命令 continue，break 来跳过循环。
+continue 用于跳过该次循环，
+break 则是用于退出循环，此外"判断条件"还可以是个常值，表示循环必定成立。
+具体用法如下：
+'''
+i = 1
+while i < 11:
+    i += 1
+    if i%2 > 0:     # 非双数时跳过输出
+        continue
+    print i         # 输出双数2、4、6、8、10
+
+'''
+
+'''
+i = 1
+while 1:            # 循环条件为1必定成立
+    print i         # 输出1~10
+    i += 1
+    if i > 10:     # 当i大于10时跳出循环
+        break
+'''
+
+3、无限循环
+如果条件判断语句永远为 true，循环将会无限的执行下去，如下实例：
+
+'''
+#!/usr/bin/python
+
+var = 1
+while var == 1 :  # 该条件永远为true，循环将无限执行下去
+   num = raw_input("Enter a number  :")
+   print "You entered: ", num
+
+print "Good bye! python 2017"
+'''
+
+注意：
+        以上的无限循环你可以使用 CTRL+C 来中断循环。
+
+4、循环使用 else 语句
+      在 python 中，for … else 表示这样的意思，for 中的语句和普通的没有区别，else 中的语句会在循环正常执行完
+      （即 for 不是通过 break 跳出而中断的）的情况下执行，while … else 也是一样。
+例子：
+
+'''
+count = 0
+while count < 15:
+   print count, " is  less than 15"
+   count = count + 1
+else:
+   print count, " is not less than 15"
+'''
+5、简单语句组
+类似if语句的语法，如果你的while循环体中只有一条语句，你可以将该语句与while写在同一行中， 如下所示：
+
+'''
+#!/usr/bin/python
+
+flag = 10
+while (flag): print 'Given flag is really true!'
+print "Good bye!python 2017"
+'''
+注意：
+       以上的无限循环你可以使用 CTRL+C 来中断循环。
+--------------------------------------------------------------------------------------------------------------------------
 while循环: 一直循环执行语句,注意缩进
 #注意缩进
 while 判断条件:
