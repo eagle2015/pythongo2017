dbc4459b0ba3e1304a327f0e7213a197632fe3a6
eagle2015 <uttflying@foxmail.com>
2017-02-19 17:24:54 +0800

    python

--- python基础/01.python基础/02.Python基本语法.py
+++ python基础/01.python基础/02.Python基本语法.py
@@ -45,3 +45,63 @@ Type "help", "copyright", "credits" or "license" for more information.
     四、Python的行和缩进
      学习Python与其他语言最大的区别就是，Python的代码块不使用大括号（{}）来控制类，函数以及其他逻辑判断。python最具特色的就是用缩进来写模块。
      缩进的空白数量是可变的，但是所有代码块语句必须包含相同的缩进空白数量，这个必须严格执行。
+
+在Python的代码块中必须使用相同数目的行首缩进空格数。
+
+五、Python的多行语句
+        Python语句中一般以新行作为为语句的结束符。但是我们也可以使用斜杠（ \）将一行的语句分为多行显示，如下所示：
+'''
+total = item_one + \
+       item_two + \
+       item_three
+'''
+
+语句中包含[], {} 或 () 括号就不需要使用多行连接符。如下实例：
+days = ['Monday', 'Tuesday', 'Wednesday',
+        'Thursday', 'Friday']
+
+六、Python 引号
+Python 接收单引号(' )，双引号(" )，三引号(''' """) 来表示字符串，引号的开始与结束必须的相同类型的。
+其中三引号可以由多行组成，编写多行文本的快捷语法，常用语文档字符串，在文件的特定地点，被当做注释。
+
+七、Python注释
+1、在python中单行注释采用 # 开头。
+2、python没有块注释，所以现在推荐的多行注释也是采用的 #。
+
+
+
+八、Python的空行
+       函数之间或类的方法之间用空行分隔，表示一段新的代码的开始。类和函数入口之间也用一行空行分隔，以突出函数入口的开始。
+       空行与代码缩进不同，空行并不是Python语法的一部分。书写时不插入空行，Python解释器运行也不会出错。
+       但是空行的作用在于分隔两段不同功能或含义的代码，便于日后代码的维护或重构。
+       记住：空行也是程序代码的一部分。
+
+九、Python实现等待用户输入
+下面的程序在按回车键后就会等待用户输入:raw_input
+
+#!/usr/bin/python
+
+raw_input("\n\nPress the enter key to exit.")
+
+
+十、Python同一行显示多条语句
+Python可以在同一行中使用多条语句，语句之间使用分号(;)分割。
+以下是一个简单的实例：
+
+import sys; x = 'foo'; sys.stdout.write(x + '\n')
+
+十一、Python多个语句构成代码组
+        缩进相同的一组语句构成一个代码块，我们称之代码组。像if、while、def和class这样的复合语句，首行以关键字开始，以冒号( : )结束，该行之后的一行或多行代码构成代码组。
+        我们将首行及后面的代码组称为一个子句(clause)。
+如下实例：if
+
+if expression :
+   suite
+elif expression :
+   suite
+else :
+   suite
+
+十二、Python命令行参数
+很多程序可以执行一些操作来查看一些基本信息，Python可以使用-h参数查看各参数帮助信息：
+
